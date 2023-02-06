73098b8c80d64ffb1928d94241d70f78d55e7d11
Takayuki Iizumi <takayukiiizumi@hotmail.com>
2023-02-06 15:39:47 +0900

    掲示板BbsDao修正

--- src/main/java/dao/BbsDao.java
+++ src/main/java/dao/BbsDao.java
@@ -2,9 +2,11 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,16 +14,18 @@ import domain.Kakikomi;
 
 public class BbsDao {
 
+	final String jdbcId = "root";
+	final String jdbcPass = "";
+	final String jdbcUrl = "jdbc:mysql://localhost:3306/board?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Asia/Tokyo";
+
+	
 	// 投稿リストを取得
 	public List<Kakikomi> findAll() {
 		System.out.println("データベースから投稿リストを取得");
 
 		List<Kakikomi> list = new ArrayList<>();
 
-		final String jdbcId = "root";
-		final String jdbcPass = "";
-		final String jdbcUrl = "jdbc:mysql://localhost:3306/board?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Asia/Tokyo";
-
+		
 		Connection con = null;
 
 		try {
@@ -86,6 +90,45 @@ public class BbsDao {
 		System.out.println(kakikomi.getName());
 		System.out.println(kakikomi.getComment());
 
-	}
+		Connection con = null;
 
+		try {
+
+			con = DriverManager.getConnection(jdbcUrl, jdbcId, jdbcPass);
+
+			System.out.println("Connected....");
+
+			try {
+				Statement st = con.createStatement();
+				String sql = "insert into bbs "
+				+ " (id, name, comment, date)"
+				+ " VALUES (?, ?, ?, NOW())";
+				PreparedStatement stmt = con.prepareStatement(sql);
+				stmt.setObject(1, kakikomi.getId(), Types.INTEGER);
+				stmt.setString(2, kakikomi.getName());
+				stmt.setString(3, kakikomi.getComment());
+				stmt.executeUpdate();
+			} catch (Exception e) {
+				throw e;
+			}
+
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Connection Failed.");
+			
+		} finally {
+
+			if (con != null) {
+				try {
+					con.close();
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
+
